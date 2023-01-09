a94c5cf09336a2d6d2039d23aadc7f0787331645
bortomar <bortomar@email.cz>
2023-01-09 07:44:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript-playground
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/h4sat8)
---
+++ index.ts
@@ -0,0 +1,64 @@
+// Welcome to the TypeScript Playground, this is a website
+// which gives you a chance to write, share and learn TypeScript.
+
+// You could think of it in three ways:
+//
+//  - A location to learn TypeScript where nothing can break
+//  - A place to experiment with TypeScript syntax, and share the URLs with others
+//  - A sandbox to experiment with different compiler features of TypeScript
+
+
+
+function test(a:number, b: number): number {
+    return a + b;
+}
+
+test(3, 4);
+
+let foo: {
+    bar: number
+};
+
+foo = {
+    //foo: 4,
+    bar: NaN
+};
+
+let a: (number | null)[] = [ 1, 2, null, 3];
+
+function check(x: number = 1, a: any, b?: number): (boolean[] | never) {
+    if (typeof a === 'number' || x > 5) {
+        return [true];
+    }
+    throw new Error('xxx');
+}
+
+//check(undefined, '4', 1)
+
+class Bar {
+    n: number;
+    constructor(h: number, public x: string) {
+        this.n = h;
+        //this.x = x;
+    }
+    hello(): void {
+        console.log(`hello ${this.n}`)
+
+    }
+}
+
+function Foo(this: any, h: number) {
+    this.n = h;
+    this.hello  = (): void => console.log(`hello ${this.n}`)
+}
+
+const fb = new (Foo as any)(2);
+fb.hello()
+
+console.log(Bar.prototype.hello)
+
+const b = new Bar(1, '5');
+console.log(b.x)
+// To learn more about the language, click above in "Examples" or "What's New".
+// Otherwise, get started by removing these comments and the world is your playground.
+  
